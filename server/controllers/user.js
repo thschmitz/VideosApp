37fb5b233690cbfa95a7008c4de7ea9ca9a82017
thschmitz <thomas.henrique.schmitz@gmail.com>
2022-10-01 15:18:26 -0300

    TokenVerifyingMethod

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,72 @@
-export const test = (req, res) => {
-    res.json("Successfull")
+import { createError } from "../error.js";
+import User from "../models/User.js";
+
+export const updateUser = async(req, res, next) => {
+    if(req.params.id === req.user.id) {
+        try{
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            }, {new:true})
+
+            res.status(200).json(updatedUser)
+        }catch(err){
+            next(err)
+        }
+    } else {
+        return next(createError(403, "You can delete only your account!"))
+    }
+}
+
+export const deleteUser = async (req, res, next) => {
+    if (req.params.id === req.user.id) {
+        try {
+          await User.findByIdAndDelete(req.params.id);
+          res.status(200).json("User has been deleted.");
+        } catch (err) {
+          next(err);
+        }
+    } else {
+        return next(createError(403, "You can delete only your account!"));
+    }
+}
+
+export const getUser = async(req, res, next) => {
+    try{
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user)
+    }catch(err) {
+        next(err)
+    }
+}
+
+export const subscribe = async(req, res, next) => {
+    try{
+
+    }catch(err) {
+        next(err)
+    }
+}
+
+export const unsubscribe = async(req, res, next) => {
+    try{
+
+    }catch(err) {
+        next(err)
+    }
+}
+
+export const like = async(req, res, next) => {
+    try{
+
+    }catch(err) {
+        next(err)
+    }
+}
+
+export const dislike = async (req, res, next) => {
+    try{
+
+    }catch(err) {
+        next(err)
+    }
 }
