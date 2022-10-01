58b4424763e9dea962ccbace34d8b9feb68a035d
thschmitz <thomas.henrique.schmitz@gmail.com>
2022-10-01 15:18:53 -0300

    VerifyTokenMethod

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -3,6 +3,5 @@ import { } from "../controllers/comment.js";
 
 const router = express.Router();
 
-router.get("/test", )
 
 export default router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,28 @@
 import express from "express";
-import { test } from "../controllers/user.js";
+import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, updateUser } from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
-router.get("/test", test)
+// update a user
+router.put("/:id", verifyToken, updateUser)
+
+// delete a user
+router.delete("/:id", verifyToken, deleteUser)
+
+// get a user
+router.get("/:id", getUser)
+
+// subscribe a user
+router.put("/sub/:id", verifyToken, subscribe)
+
+// unsubscribe a user
+router.put("/sub/:id", verifyToken, unsubscribe)
+
+// like a video
+router.put("/like/:videoId", verifyToken, like)
+
+// dislike a video
+router.put("/like/:videoId", verifyToken, dislike)
 
 export default router;
