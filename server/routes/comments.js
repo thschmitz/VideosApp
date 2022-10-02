51c5592dd2161cbf1145b5ea11c835282024176a
thschmitz <thomas.henrique.schmitz@gmail.com>
2022-10-02 16:31:17 -0300

    BackEndCompleted

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { } from "../controllers/comment.js";
+import { addComment, deleteComment, editComment, getComment } from "../controllers/comment.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
+router.post("/", verifyToken, addComment)
+router.get("/:videoId", getComment)
+router.delete("/:videoId", verifyToken, deleteComment)
+router.put("/:videoId", verifyToken, editComment)
 
 export default router;
